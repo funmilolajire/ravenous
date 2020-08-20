677ae8cb38acf471df22a959b11a6b50edee4a6f
Funmilola O <funmilolajire@gmail.com>
2020-08-20 22:53:52 +0100

    Updates

---
+++ precache-manifest.11a59387dabaa50aceb317856857ff2b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9401c68e8a2500b9bbfac0e68c3be709",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5a1add622541c1d6363",
+    "url": "/static/css/main.09cfcc26.chunk.css"
+  },
+  {
+    "revision": "0a84b5209ff9a9e080b8",
+    "url": "/static/js/2.168dd5df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.168dd5df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5a1add622541c1d6363",
+    "url": "/static/js/main.d2a38671.chunk.js"
+  },
+  {
+    "revision": "0b7eb9d469061bce3505",
+    "url": "/static/js/runtime-main.140093ed.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
