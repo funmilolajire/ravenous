742edb2a14d41485ea40e991e516a10df618a643
Funmilola O <funmilolajire@gmail.com>
2020-08-20 21:39:41 +0100

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import { SearchBar } from '../SearchBar/SearchBar';
+import { BusinessList } from '../BusinessList/BusinessList';
+import { Yelp } from '../../util/Yelp';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      businesses: []
+    }
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
+  async searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp for ${term}, ${location}, ${sortBy}`);
+    Yelp.search(term, location, sortBy).then(businesses =>
+      this.setState({ businesses: businesses })
+    )
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={this.state.businesses} />
+      </div>
+    );
+  }
+}
+
